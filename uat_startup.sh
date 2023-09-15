361d3b35d0b7ff70285d04b58423d363e05e4650
psivaramps <93992293+psivaramps@users.noreply.github.com>
2023-09-15 23:31:53 +0530

    Update uat_startup.sh

--- uat_startup.sh
+++ uat_startup.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+yum update -y
+yum install -y httpd 
+systemctl start httpd
+systemctl enable httpd
+echo "<h1> User-Acceptance-Test ! Welcome to MI Institute </h1>" > /var/www/html/index.html
