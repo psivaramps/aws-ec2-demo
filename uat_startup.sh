c9fc3f5dd4630d1575bb767079ab0cedf01b54a9
psivaramps <93992293+psivaramps@users.noreply.github.com>
2023-09-15 23:43:02 +0530

    Update uat_startup.sh

--- uat_startup.sh
+++ uat_startup.sh
@@ -3,4 +3,4 @@ yum update -y
 yum install -y httpd 
 systemctl start httpd
 systemctl enable httpd
-echo "<h1> User-Acceptance-Test ! Welcome to MI Institute </h1>" > /var/www/html/index.html
+echo "<h1> User-Acceptance-Test Environment ! Welcome to MI Institute </h1>" > /var/www/html/index.html
