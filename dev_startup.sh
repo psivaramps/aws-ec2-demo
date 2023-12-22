a8d645428a7ce5bac9f98cfa9c3da39d3eebec92
psivaramps <93992293+psivaramps@users.noreply.github.com>
2023-12-22 07:43:19 +0530

    Update dev_startup.sh

--- dev_startup.sh
+++ dev_startup.sh
@@ -3,4 +3,5 @@ yum update -y
 yum install -y httpd 
 systemctl start httpd
 systemctl enable httpd
+yum update -y
 echo "<h1> Developer-Environment ! Welcome to MI Institute </h1>" > /var/www/html/index.html
